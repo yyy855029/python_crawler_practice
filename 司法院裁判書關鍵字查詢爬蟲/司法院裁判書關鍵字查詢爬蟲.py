dd9ec38ebef2489fe651dc6cc2030fb482a51119
HUANG,ZI-WEI <53975110+yyy855029@users.noreply.github.com>
2020-09-16 23:34:21 +0800

    fifth commit

--- 司法院裁判書關鍵字查詢爬蟲/司法院裁判書關鍵字查詢爬蟲.py
+++ 司法院裁判書關鍵字查詢爬蟲/司法院裁判書關鍵字查詢爬蟲.py
@@ -7,16 +7,54 @@ import pandas as pd
 from tqdm import tqdm
 import time
 import json
+import re
 
 #隱藏warning
 requests.packages.urllib3.disable_warnings()
 
 
 #定義去除unicode字串函數
-def process(input_str):
+def process_content(input_str):
     return ''.join([c if ord(c)<=55291 else '_' for c in input_str])
 
 
+#定義轉換民國年到西元年字串函數
+def process_time(input_str):
+    return '/'.join([str(int(e)+1911) if i==0 else e for i,e in enumerate(input_str.split('.'))])
+
+
+#定義區分法院類別字串函數
+def process_court(input_str):
+    court_name=input_str.split(' ')[0]
+    special_courts=['公務員懲戒委員會','智慧財產法院']
+    for e in special_courts:
+        if e in court_name:
+            return e
+    if '福建' in court_name:
+        return court_name[2:6]
+    else:
+        return court_name[-4:]
+
+    
+#定義區分地點字串函數
+def process_location(input_str):
+    if '福建' in input_str:
+        return '金門'
+    elif '地方' in input_str:
+        return input_str.split(' ')[0][2:4]
+    elif '高等' in input_str:
+        if '分院' in input_str:
+            return input_str.split(' ')[1][:2]
+        elif '行政' in input_str:
+            return input_str[:2]
+        #臺灣高等法院(後面沒有接分院)
+        else:
+            return '臺北'
+    #公務員懲戒委員會、智慧財產法院
+    else:
+        return '臺北'
+
+    
 #定義司法院裁判書關鍵字查詢函數
 def law_crawler(name):
     url='https://law.judicial.gov.tw/FJUD/default.aspx'
@@ -53,7 +91,7 @@ def law_crawler(name):
 
     #紀錄總判決頁數，處理頁數可能只有一頁的情況
     try:
-        pages=int(soup.select('div.pull-right')[0].text.split(' / ')[1].split(' 頁')[0])
+        pages=int(re.findall(r'/\s(\d+)\s',soup.select('div.pull-right')[0].text)[0])
     except:
         pages=1
     
@@ -69,20 +107,31 @@ def law_crawler(name):
         try:
             df=pd.read_html(response.text)[0]
             Names=pd.Series([name]*len(df[df.index%2!=0]))
-            Contents=df[df.index%2!=0]['裁判日期'].apply(lambda x:process(x)).reset_index(drop=True)
-            CaseNos=df[df.index%2==0]['裁判字號 （內容大小）'].apply(lambda x:x.split('第 ')[1].split(' 號')[0]).reset_index(drop=True).astype(int)
-            Types=df[df.index%2==0]['裁判案由'].reset_index(drop=True)
-            Times=df[df.index%2==0]['裁判日期'].reset_index(drop=True)
-            Locations=df[df.index%2==0]['裁判字號 （內容大小）'].apply(lambda x:x[2:4] if ('地方法院' in x) else x[7:9]).str.replace('10','臺北').reset_index(drop=True)
+            Contents=df[df.index%2!=0]['裁判日期'].apply(lambda x:process_content(x)).reset_index(drop=True)
+            Times=df[df.index%2==0]['裁判日期'].apply(lambda x:process_time(x)).reset_index(drop=True)
             Titles=df[df.index%2==0]['裁判字號 （內容大小）'].reset_index(drop=True)
+            CaseNos=df[df.index%2==0]['裁判字號 （內容大小）'].apply(lambda x:int(re.findall(r'第 (\d+) 號',x)[0])).reset_index(drop=True)
+            Sizes=df[df.index%2==0]['裁判字號 （內容大小）'].apply(lambda x:re.findall(r'（(\w+)）',x)[0]).reset_index(drop=True)
+            Types=df[df.index%2==0]['裁判字號 （內容大小）'].apply(lambda x:re.findall(r'號(\w{2})',x)[0]).reset_index(drop=True)
+            Judges=df[df.index%2==0]['裁判字號 （內容大小）'].apply(lambda x:re.findall(r'號\w{2}(\w{2})（',x)[0]).reset_index(drop=True)
+            Words=df[df.index%2==0]['裁判字號 （內容大小）'].apply(lambda x:re.findall(r'度\s(\w+)\s字',x)[0]).reset_index(drop=True)
+            Courts=df[df.index%2==0]['裁判字號 （內容大小）'].apply(lambda x:process_court(x)).reset_index(drop=True)
+            Locations=df[df.index%2==0]['裁判字號 （內容大小）'].apply(lambda x:process_location(x)).reset_index(drop=True)
+            Causes=df[df.index%2==0]['裁判案由'].reset_index(drop=True)
+            
             #建立小表格
             df=pd.DataFrame({'name':Names,
                              'time':Times,
                              'location':Locations,
+                             'court':Courts,
                              'caseNo':CaseNos,
                              'type':Types,
-                             'content':Contents,
-                             'title':Titles})
+                             'judge':Judges,
+                             'word':Words,
+                             'cause':Causes,
+                             'size':Sizes,
+                             'title':Titles,
+                             'content':Contents})
             dfs.append(df)
             time.sleep(0.5)
         except:
@@ -92,16 +141,15 @@ def law_crawler(name):
     #改成字典格式
     data_list=df.to_dict(orient='records')
         
-        
     return df,data_list
 
 
 #抓取指定司法院裁判書關鍵字查詢結果
-df,data_list=law_crawler('王小明')
+df,data_list=law_crawler('蔡英文')
 #將指定司法院裁判書關鍵字查詢結果匯出成Excel檔
-df.to_excel('王小明.xlsx',index=False)
+df.to_excel('蔡英文.xlsx',index=False)
 #將指定司法院裁判書關鍵字查詢結果匯出成Json檔
-with open('王小明.json','w') as outfile:
+with open('蔡英文.json','w',encoding='utf8') as outfile:
     json.dump(data_list,outfile,ensure_ascii=False)
 
 
